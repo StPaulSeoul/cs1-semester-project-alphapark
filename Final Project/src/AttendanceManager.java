0d79941e6e6aa57bbfe61631583f4c2fb29e9797
jeffpark <jeffpark@192.168.0.149>
2016-12-15 10:45:10 +0900

    Sorry for late commit.

---
+++ Final Project/src/AttendanceManager.java
@@ -0,0 +1,15 @@
+
+	import java.util.Scanner;
+	public class AttendanceManager {
+		public static void main (String[]args){
+			Scanner buttons = new Scanner (System.in);
+			int Student1, Student2, Student3;
+			System.out.print("Student Name");
+			
+			System.out.printf(" #%d"  );
+			
+		}
+
+	}
+
+
